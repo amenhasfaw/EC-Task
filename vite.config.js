3c6ab8a805bdbb3b2d0ffd10bb2c930589f23dfe
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2023-07-25 01:26:52 +0300

    Update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"{EC-Task}"
+  base:"{EC-Task}",
   plugins: [react()],
   esbuild: {
     loader: "tsx", // OR "jsx"
